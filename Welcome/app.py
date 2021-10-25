b7e029423a41a82f3d9fb6a579576d4239e5e92a
NeoArte <nicolasdestito74@gmail.com>
2021-10-25 20:27:23 -0300

    Comments are now all in English

--- Welcome/app.py
+++ Welcome/app.py
@@ -1,26 +1,30 @@
 from flask import Flask
 
 
-# --- --- --- --- --- ---  Definimos nosso app flask --- --- --- --- --- --- 
+# --- --- --- --- --- ---  Create our flask app --- --- --- --- --- --- 
 
 app = Flask(__name__)
 
-# Flask(__name__) will print ("<Flask 'app'>"), sendo que 'app' não se refere ao nome da variável, mas aquele do arquivo que está rodando.
+# Flask(__name__) will print ("<Flask 'app'>"), 'app' here refers to the file (app.py) and not the variable name (app)
 # print(app) --> <Flask 'app'>"
 # print(__name__) --> __main__
 # print(Flask(__name__)) --> <Flask 'app'>"
 
-# --- --- --- --- --- --- Especificamos o que ocorre em nosso app --- --- --- --- --- --- 
+# --- --- --- --- --- --- Defines what happens on the flask app --- --- --- --- --- --- 
 
-@app.route('/') # app.route define a rota em que aquele método está rodando, seria como o urlpatterns do Django
+# GET requests like these (that makes the pages actually be acessed) are handled by Flask
+@app.route('/') # app.route sets the route at which the funciont reffers to (like the urlpattern in Django that defines a url and a views function)
 def welcome():
-    return home() # Retornando outra função (funções de página) é possivel redirecionar o usuário
+    return home() # If we return another function and not text, we can make the action of redirecting the users (apparently)
 
 @app.route('/home')
 def home():
     return 'This is my first flask app! <3 You were redirect from the url /'
 
+@app.route('/users')
+def users():
+    return 'Here you can find all of our user database! :D'
 
-# --- --- --- --- --- --- Rodamos nosso app flask --- --- --- --- --- --- 
+# --- --- --- --- --- --- Run the flask app --- --- --- --- --- --- 
 
-app.run() # Roda o aplicativo flash
+app.run() # Runs our flask app
