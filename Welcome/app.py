4286d9d1082aec65aef17563b81c4ed81db98c86
NeoArte <nicolasdestito74@gmail.com>
2021-10-25 20:43:29 -0300

    methods GET and POST example added

--- Welcome/app.py
+++ Welcome/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 
 # --- --- --- --- --- ---  Create our flask app --- --- --- --- --- --- 
@@ -12,7 +12,8 @@ app = Flask(__name__)
 
 # --- --- --- --- --- --- Defines what happens on the flask app --- --- --- --- --- --- 
 
-# GET requests like these (that makes the pages actually be acessed) are handled by Flask
+# GET requests like these (that makes the pages actually be acessed) are handled by Flask, so by default they are done.
+
 @app.route('/') # app.route sets the route at which the funciont reffers to (like the urlpattern in Django that defines a url and a views function)
 def welcome():
     return home() # If we return another function and not text, we can make the action of redirecting the users (apparently)
@@ -24,6 +25,17 @@ def home():
 @app.route('/users')
 def users():
     return 'Here you can find all of our user database! :D'
+
+
+# Dealing with methods manually
+
+# @app.route('/method', methods=['GET']) --> Same behavior as just declaring without methods as before 
+@app.route('/method', methods=['GET', 'POST'])
+def method():
+    if request.method == "POST":
+        return "Wow that is a POST method! That is cool! BD"
+    elif request.method == "GET":
+        return "Wow that is a GET method! That is cool! BD"
 
 # --- --- --- --- --- --- Run the flask app --- --- --- --- --- --- 
 
