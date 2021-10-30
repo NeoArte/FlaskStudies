002635329ab403ddeaab299ae8518aefb3a595aa
NeoArte <nicolasdestito74@gmail.com>
2021-10-29 21:55:12 -0300

    Showing formated bmi result

--- 3-BmiCalculator/app.py
+++ 3-BmiCalculator/app.py
@@ -13,19 +13,39 @@ def index():
     # BMI is measured in Kg/M², the equation for it is Mass / Height^2
     if request.method == "POST":
 
+        weight = request.form.get('weight')
+        height = request.form.get('height')
+
         # We must verify if somehow the user sent a field with no value ("") and if that is the case counter it.
         if weight == '' or height == '':
             print('weight: ', weight=='')
             print('height: ', height=='')
             return render_template('index.html',
                                     missing_weight=weight == '',
-                                    missing_height=height == '')
+                                    missing_height=height == '',
+                                    result_available=False)
 
-        weight = float(request.form.get('weight'))
-        height = int(request.form.get('height'))
+        weight = float(weight)
+        height = int(height)/100
         bmi = weight / (height**2)
+        bmi_level = "blue" # Severe Thinness & Moderate Thinness > blue | Mild Thinness & Normal > green |
+                           # Overweight & Obese I > yellow | Obese II & Obese III > red
+        bmi_explanation = \
+            '''
+            
+            '''
+
+        if bmi <= 25 and bmi > 17:
+            bmi_level = 'green'
+        elif bmi <= 35 and bmi > 25:
+            bmi_level = 'yellow'
+        elif bmi > 35:
+            bmi_level = 'red'
+        
+
+        return render_template('index.html', bmi=f"{bmi:.2f}", level=bmi_level, result_available=True)
     
-    return render_template('index.html')
+    return render_template('index.html', result_available=False)
 
 # Running our app -------------------
 
