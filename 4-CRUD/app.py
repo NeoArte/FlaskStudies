71362017dc1c269af76b0b04c954d1911e1d9123
NeoArte <nicolasdestito74@gmail.com>
2022-03-07 18:40:08 -0300

    Sign Up HTML and sign_up() logic done (no database actions)

--- 4-CRUD/app.py
+++ 4-CRUD/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, render_template, redirect
 
 app = Flask(__name__)
 
@@ -6,6 +6,22 @@ app = Flask(__name__)
 def index():
     """."""
     pass
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def sign_up():
+    """
+    Sign up route method.
+
+    Returns a page for GET and process new user for POST request.
+    """
+    if request.method == "POST":
+        cpf = request.form.get('cpf')
+        name = request.form.get('name')
+        cep = request.form.get('cep')
+        return redirect("/")
+
+    return render_template("sign_up.html")
 
 
 app.run()
